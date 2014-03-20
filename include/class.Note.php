aa7d13230203bc81b56a438e08d313d9cc9a6788
Chuck Werner <mistergone@gmail.com>
2014-03-19 21:23:57 -0400

    Playing around...

---
+++ include/class.Note.php
@@ -0,0 +1,13 @@
+<?php
+
+class Note {
+	var $id;
+	var $person_id;
+	var $timestamp;
+	var $content;
+}
+
+
+
+
+?>
---
+++ include/class.Person.php
@@ -0,0 +1,15 @@
+<?php
+
+class Person {
+	var $id;
+	var $name;
+	var $family;
+	var $city;
+	var $job;
+	var $parent;
+	var $descendents;
+
+}
+
+
+?>
